d4a7cf73781476f9bde92eae546b863653bae29b
iclectic <tansonestost@yahoo.com>
2019-08-23 15:16:52 -0700

    Liquid image transition in javascript , inspired by Dev Ed's youtube tutorial

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+   parent: document.querySelector('.distortion'),
+   intensity: 0.4,
+   image1: './images/bsg.jpg',
+   image2: './images/bd.jpg',
+   displacementImage: "./images/hm.png"
+});
